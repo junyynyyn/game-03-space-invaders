e7e343832dbc9825b853004284c3227d54dbbb98
Jun <junyynyyn@users.noreply.github.com>
2023-12-27 09:56:21 +0800

    First Commit

---
+++ Scenes/player.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _physics_process(delta):
+	# Add the gravity.
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	if (Input.is_action_just_pressed("fire")):
+		print("Pew pew")
+
+	move_and_slide()
